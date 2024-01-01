db902a6265e6698353d631c2e2bd678cb30070be
Thiagomuniz012 <thiago.muniz012@gmail.com>
2024-01-01 19:04:56 -0300

    Subindo base

---
+++ src/lib/Logo.svelte
@@ -0,0 +1,7 @@
+<img src="logo.png" alt="Logo do Site" class="mr-10">
+
+<style>
+    img{
+        height: 2rem;
+    }
+</style>
---
+++ src/routes/Faq/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    /**
+   * @type {{ name: any; } | null}
+   */
+    let data = null;
+  
+    async function fetchPokemonData() {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+        data = await response.json();
+      } catch (err) {
+        console.error('Erro:', err);
+      }
+    }
+  
+    fetchPokemonData();
+  </script>
+
+  {#if data}
+    <p>Nome do Pokémon: {data.name}</p>
+  {:else}
+    <p>Carregando...</p>
+  {/if}
+  
